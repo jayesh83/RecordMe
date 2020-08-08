a336d1019804c70bb5f8eaf955f5e6fa3c6d92bf
Jayesh Suthar <40509714+jayesh83@users.noreply.github.com>
2020-08-08 11:31:00 +0530

    updated RecorderService

--- app/src/main/java/com/japps/recordme/RecorderService.kt
+++ app/src/main/java/com/japps/recordme/RecorderService.kt
@@ -5,11 +5,13 @@ import android.content.Context
 import android.content.Intent
 import android.media.MediaRecorder
 import android.os.Build
+import android.os.IBinder
 import android.util.Log
 import java.io.IOException
 
-open class RecorderService(name: String? = RecorderService::class.simpleName) :
-    IntentService(name) {
+open class RecorderService(
+    name: String? = RecorderService::class.simpleName
+) : IntentService(name) {
     private lateinit var mediaRecorder: MediaRecorder
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -17,21 +19,24 @@ open class RecorderService(name: String? = RecorderService::class.simpleName) :
         val outputFile = "${externalCacheDir?.absolutePath}/audiorecordtest.3gp"
         Log.e("Service", "output -> $outputFile")
         mediaRecorder.apply {
-            setAudioSource(MediaRecorder.AudioSource.VOICE_CALL)
-            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB)
+            setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION)
+            setOutputFormat(MediaRecorder.OutputFormat.AMR_NB)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
             setOutputFile(outputFile)
         }
 
         try {
             mediaRecorder.prepare()
-            Thread.sleep(1000)
+            Thread.sleep(2000)
             mediaRecorder.start()
         } catch (ise: IllegalStateException) {
             Log.e("Service", "Exception -> $ise")
         } catch (ioe: IOException) {
             Log.e("Service", "Exception -> $ioe")
+        } catch (exe: Exception) {
+            Log.e("Service", "Exception -> $exe")
         }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createNotificationChannel()
             val notification = Notification.Builder(this, "default_channel")
@@ -44,18 +49,15 @@ open class RecorderService(name: String? = RecorderService::class.simpleName) :
         return Service.START_STICKY
     }
 
-
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name: CharSequence = getString(R.string.app_name)
             val importance = NotificationManager.IMPORTANCE_LOW
-//            val channelId: String = CorUtility.Companion.getNotificationChannel()
             val channelId = "default_channel"
             val channel = NotificationChannel(channelId, name, importance)
             channel.enableLights(false)
             channel.setSound(null, null)
             channel.setShowBadge(false)
-//            channel.description = SyncStateContract.Constants.NOTIFICATION_DESC
             val notificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
@@ -72,5 +74,7 @@ open class RecorderService(name: String? = RecorderService::class.simpleName) :
     override fun onHandleIntent(intent: Intent?) {
         Log.e("Service", "OnHandleIntent")
     }
+
+    override fun onBind(intent: Intent?): IBinder? = null
 
 }
